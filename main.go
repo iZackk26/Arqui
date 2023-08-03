7fb82844251bfdf4e410daf9b32a18778806d22e
iZackk26 <newxstorecr@gmail.com>
2023-08-03 15:49:33 -0600

    Code

---
+++ main.go
@@ -0,0 +1,200 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type CPU struct {
+	accumulator  int64
+	instructions *bytes.Buffer
+	memory       [2048]int64
+}
+
+func (c *CPU) loadInstructions(filename string) error {
+    instructions := &bytes.Buffer{}
+	file, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		temp := strings.Fields(line)
+		for _, token := range temp {
+			num, err := strconv.ParseInt(token, 2, 64)
+			if err != nil {
+				return err
+			}
+
+            binary.Write(instructions, binary.LittleEndian, num)
+		}
+	}
+	c.instructions = instructions
+	return nil
+}
+
+func (c *CPU) loadMemory(filename string) error {
+	var memory [2048]int64
+	file, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		temp := strings.Fields(line)
+
+		address, err := strconv.ParseInt(temp[0], 2, 64)
+		if err != nil {
+			return err
+		}
+		value, err := strconv.ParseInt(temp[1], 2, 64)
+		if err != nil {
+			return err
+		}
+		memory[address] = value
+	}
+	c.memory = memory
+	return nil
+}
+
+func (c *CPU) fetch() int64 {
+	var instruction int64
+	binary.Read(c.instructions, binary.LittleEndian, &instruction)
+	return instruction
+}
+
+func (c *CPU) printMemory() {
+	for i := 0; i < len(c.memory); i++ {
+		if c.memory[i] != 0 {
+			fmt.Printf("Memory[%d]: %d\n", i, c.memory[i])
+		}
+        // fmt.Printf("Memory[%d]: %d\n", i, c.memory[i])
+	}
+}
+
+func (c *CPU) print() {
+	fmt.Printf("Accumulator: %d\n", c.accumulator)
+	// fmt.Printf("Instructions: %d\n", c.instructions)
+	c.printMemory()
+	fmt.Println()
+}
+
+func (c *CPU) run() {
+	for {
+		instruction := c.fetch()
+		if instruction == 0 {
+			break
+		}
+		c.execute(instruction)
+		fmt.Printf("Instruction: %d\n", instruction)
+		c.print()
+	}
+}
+
+func (c *CPU) execute(instruction int64) error {
+	switch instruction {
+	case 1:
+		var memory int64
+		err := binary.Read(c.instructions, binary.LittleEndian, &memory)
+		if err != nil {
+			return err
+		}
+		c.accumulator = c.memory[memory]
+		binary.Read(c.instructions, binary.LittleEndian, &memory) // skip empty bits
+
+	case 2:
+		var memory int64
+		err := binary.Read(c.instructions, binary.LittleEndian, &memory)
+		if err != nil {
+			return err
+		}
+		c.memory[memory] = c.accumulator
+		binary.Read(c.instructions, binary.LittleEndian, &memory) // skip empty bits
+
+	case 3:
+		var memory int64
+		err := binary.Read(c.instructions, binary.LittleEndian, &memory)
+		if err != nil {
+			return err
+		}
+		c.accumulator += c.memory[memory]
+		binary.Read(c.instructions, binary.LittleEndian, &memory)
+	case 4:
+		var memory1 int64
+		var memory2 int64
+		err := binary.Read(c.instructions, binary.LittleEndian, &memory1)
+		if err != nil {
+			return err
+		}
+		err = binary.Read(c.instructions, binary.LittleEndian, &memory2)
+		if err != nil {
+			return err
+		}
+		c.accumulator += c.memory[memory1] + c.memory[memory2]
+
+	case 5:
+		var memory int64
+		err := binary.Read(c.instructions, binary.LittleEndian, &memory)
+		if err != nil {
+			return err
+		}
+		c.accumulator -= c.memory[memory]
+		binary.Read(c.instructions, binary.LittleEndian, &memory)
+
+	case 6:
+		var memory1 int64
+		var memory2 int64
+		err := binary.Read(c.instructions, binary.LittleEndian, &memory1)
+		if err != nil {
+			return err
+		}
+		err = binary.Read(c.instructions, binary.LittleEndian, &memory2)
+		if err != nil {
+			return err
+		}
+		c.memory[memory2] = c.accumulator - c.memory[memory1]
+
+	case 7:
+		var memory int64
+		err := binary.Read(c.instructions, binary.LittleEndian, &memory)
+		if err != nil {
+			return err
+		}
+		c.accumulator *= c.memory[memory]
+		binary.Read(c.instructions, binary.LittleEndian, &memory)
+
+	default:
+		return fmt.Errorf("Unknown instruction: %d", instruction)
+
+	}
+	return nil
+
+}
+
+func main() {
+	var cpu CPU
+	err := cpu.loadInstructions("instructions.txt")
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	err = cpu.loadMemory("memory.txt")
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	cpu.print()
+	cpu.run()
+
+}
